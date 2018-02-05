3dd73df9c32dbf34612a9eb76e65eb57a2b6b165
Ubaidullah Khan <2271135k@student.gla.ac.uk>
2018-02-05 22:30:24 +0000

    fixed cast conversion for ex 2

--- lab1/lab1-ex2.c
+++ lab1/lab1-ex2.c
@@ -6,11 +6,11 @@ int main(void){
 	int y = 2;
 	
 	printf("x = %3.2f and y = %3.2f\n", (float)x,(float)y);
-	printf("x + y = %3.2f \n",x+y);
-	printf("x - y = %3.2f \n",x - y);
-	printf("x * y = %3.2f\n", x * y);
+	printf("x + y = %3.2f \n", (float)x+y);
+	printf("x - y = %3.2f \n", (float)x - y);
+	printf("x * y = %3.2f\n", (float)x * y);
 	printf("x / y = %3.2f\n", (float) x/y);
-	printf("y  \% \\x= %3.2f\n", (float)(y%x));
+	printf("y  %% x = %3.2f\n", (float)(y%x));
 	
 	
 	return 0;
